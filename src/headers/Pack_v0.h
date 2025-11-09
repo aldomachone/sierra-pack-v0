45b8bae6480fe7f4259d0d89e0b9487ea39090d9
aldomachone <adetonon@gmail.com>
2025-11-09 12:07:34 +0100

    Ajout stubs headers + agrégateur complet Pack_v0.h (Phase0_v0)

--- src/headers/Pack_v0.h
+++ src/headers/Pack_v0.h
@@ -1,2 +1,45 @@
 #pragma once
 #include "sierrachart.h"
+
+// === Core temps / infra ===
+#include "UtilitaireTempo.h"
+#include "UtilitaireSanitize.h"
+#include "UtilitaireCompat.h"
+
+// === I/O & MLP ===
+#include "UtilitaireMLP.h"
+
+// === DOM / Tape (Phase 0) ===
+#include "UtilitaireDom.h"
+#include "UtilitaireDomSignal.h"
+#include "UtilitaireTapePace.h"
+#include "UtilitaireTapeBVC.h"
+#include "UtilitaireTapeSweep.h"
+#include "UtilitaireTapeExhaust.h"
+
+// === Contexte (Phase 0 minutes/HTF) ===
+#include "UtilitaireVWAP.h"
+#include "UtilitaireProfile.h"
+#include "UtilitaireOHLC.h"
+#include "UtilitaireRanges.h"
+
+// === Zones / Niveaux (Phase 1, pré-créés pour éviter refactors) ===
+#include "UtilitairePivots.h"
+#include "UtilitaireFibo.h"
+#include "UtilitaireReloadZone.h"
+#include "UtilitaireFVG.h"
+#include "UtilitaireZones.h"
+#include "UtilitaireConfluence.h"
+#include "UtilitaireMTF.h"
+#include "UtilitaireOverlay.h"
+
+// === Mémoire / Risque / Réponse (Phase 2) ===
+#include "UtilitaireMDHG.h"
+#include "UtilitairePRF.h"
+#include "UtilitaireLiqRisk.h"
+
+// === Algos DOM avancés (réserves, optionnels) ===
+#include "UtilitaireDomSignalMomentum.h"
+#include "UtilitaireDomImbalSweep.h"
+#include "UtilitaireDomVacuum.h"
+#include "UtilitaireStopRun.h"
